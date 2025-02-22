4829df4c5b065d269de749235d6e75798144600a
Kaπ <Kap1_priv@proton.me>
2025-02-22 17:22:23 +0100

    task 7
    done

--- Lab1/Main/src/task/Task7.java
+++ Lab1/Main/src/task/Task7.java
@@ -6,16 +6,32 @@ import java.util.Random;
 
 public class Task7 {
     public static boolean RightTriangle() {
-        System.out.print("Podaj A: ");
-        int a = Input.IntInput();
+        System.out.println("Podaj przedzial losowania liczb");
+        System.out.print("Od: ");
+        int first = Input.IntInput();
 
-        System.out.print("Podaj B: ");
-        int b = Input.IntInput();
+        System.out.print("Do: ");
+        int last = Input.IntInput();
 
+        //Losowanie z przedziału podanego prze użytkownika
         Random rand = new Random();
-        int c = rand.nextInt();
-        System.out.println(c);
+        int[] x = { 0, 0, 0 };
+        x[0] = first + rand.nextInt(last);
+        x[1] = first + rand.nextInt(last);
+        x[2] = first + rand.nextInt(last);
 
-        return true;
+        System.out.println("A = " + x[0]);
+        System.out.println("B = " + x[1]);
+        System.out.println("C = " + x[2]);
+
+        //Ktróy bok najdłuższy?
+        int longestSide = Math.max(x[0], x[1]);
+        longestSide = Math.max(longestSide, x[2]);
+
+        System.out.println("Najdłuzszy bok " + longestSide);
+
+        if(x[0] == longestSide) return (x[1] * x[1]) + (x[2] * x[2]) == longestSide;
+        else if(x[1] == longestSide) return (x[0] * x[0]) + (x[2] * x[2]) == longestSide;
+        return (x[0] * x[0]) + (x[1] * x[1]) == longestSide;
     }
 }
